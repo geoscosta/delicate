a2b7987636d53de1cf6b66c0ecbc0893a0cf60e1
Geovane Silva <geoscosta@gmail.com>
2022-03-13 15:44:45 -0300

    Projeto criado

---
+++ backend/src/main/java/com/dts/delicate/DelicateApplication.java
@@ -0,0 +1,13 @@
+package com.dts.delicate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelicateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelicateApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dts/delicate/DelicateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dts.delicate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelicateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
